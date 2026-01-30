a8effe007b2d74827f37fd32d1a22f53fb3cb415
ayanaphon <ayanaphon@gmail.com>
2026-01-30 15:33:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  /* ================= NAMA TAMU ================= */
+
+  const params = new URLSearchParams(window.location.search);
+  let tamu = params.get("tamu");
+
+  if (tamu) {
+    localStorage.setItem("namaTamu", tamu);
+  } else {
+    tamu = localStorage.getItem("namaTamu");
+  }
+
+  tamu = tamu || "Tamu Undangan";
+
+  document.querySelectorAll(".namaTamu").forEach(el => {
+    el.textContent = decodeURIComponent(tamu);
+  });
+
+
+  /* ================= OPEN INVITATION ================= */
+
   const openBtn = document.querySelector(".open-btn");
   const coverCard = document.getElementById("coverCard");
   const salamSection = document.getElementById("salamSection");
